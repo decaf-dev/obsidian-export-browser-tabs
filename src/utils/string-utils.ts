bbd0181120d24be879c93980e0e3f999d0cbf102
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2023-12-25 21:16:50 -0700

    fix: keep I uppercase

--- src/utils/string-utils.ts
+++ src/utils/string-utils.ts
@@ -3,7 +3,19 @@ export const toSentenceCase = (value: string) => {
 	const sentences = value.split(/(?<=[.!?])\s/);
 
 	// Convert each sentence to sentence case
-	return sentences.map(sentence =>
-		sentence.charAt(0).toUpperCase() + sentence.slice(1).toLowerCase()
-	).join(' ');
+	return sentences.map(sentence => {
+		// Split the sentence into words
+		const words = sentence.split(' ');
+
+		// Convert each word to the correct case
+		const correctedWords = words.map((word, index) => {
+			if (word.toLowerCase() === 'i') {
+				return 'I'; // Always capitalize 'I'
+			}
+			return index === 0 ? word.charAt(0).toUpperCase() + word.slice(1).toLowerCase() : word.toLowerCase();
+		});
+
+		// Rejoin the words into a sentence
+		return correctedWords.join(' ');
+	}).join(' ');
 }
